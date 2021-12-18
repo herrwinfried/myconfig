c48439e0ddc27c92db6588666510d76127e340b8
winfried <ozgurarslln@icloud.com>
2021-12-18 17:15:16 +0300

    beta

--- mass.ps1
+++ mass.ps1
@@ -3,18 +3,20 @@ if (Test-Path -Path $env:TEMP/mass.zip) {
     Remove-Item $env:TEMP/mass.zip -Recurse
 } else {
 }
+if (Test-Path -Path $env:TEMP/mass) {
+    Write-Host "Mass adında klasör buldum onu siliyorum."
+     Remove-Item $env:TEMP/mass -Recurse
+ } else {
+ }
 Write-Host "MAS indiriyorum GitHub dan"
 Invoke-WebRequest https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip -OutFile $env:TEMP/mass.zip
 Write-Host "indirdim"
-if (Test-Path -Path $env:TEMP/mass) {
-   Write-Host "Mass adında klasör buldum onu siliyorum."
-    Remove-Item $env:TEMP/mass -Recurse
-} else {
-}
+
 Write-Host "Zipten çıkartıyorum"
 Expand-Archive $env:TEMP\mass.zip -DestinationPath $env:TEMP\mass
 Write-Host "Zipten çıkarttım"
 
 Write-Host "HWID çalıştırıyorum"
-Set-Location $env:TEMP\mass\MAS_1.4\Separate-Files-Version\Activators\HWID-KMS38_Activation; .\HWID_Activation.cmd
+Set-Location $env:TEMP\mass\MAS_1.4\Separate-Files-Version\Activators\HWID-KMS38_Activation
+cmd .\HWID_Activation.cmd
 Pause
--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
 @echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
-powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Invoke-WebRequest https://raw.githubusercontent.com/herrwinfried/myconfig/mas-automatic/mass.ps1 -OutFile $env:TEMP/mass.ps1;"
-powershell.exe -noexit -ExecutionPolicy Bypass -NoProfile -Command ".\mass.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Invoke-WebRequest https://raw.githubusercontent.com/herrwinfried/myconfig/mas-automatic/mass.ps1 -OutFile $env:TEMP/mass.ps1";powershell.exe -ExecutionPolicy Bypass -NoProfile -Command powershell.exe -ExecutionPolicy Bypass -NoProfile $env:TEMP\mass.ps1
