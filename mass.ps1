b3c9a8553086a46d50c9f88b38d88683ade589d6
winfried <ozgurarslln@icloud.com>
2021-12-18 17:37:03 +0300

    beta

--- mass.ps1
+++ mass.ps1
@@ -1,10 +1,10 @@
 if (Test-Path -Path $env:TEMP/mass.zip) {
-   Write-Host "Mass adında zip buldum onu siliyorum."
+   Write-Host "Mass adinda zip buldum onu siliyorum."
     Remove-Item $env:TEMP/mass.zip -Recurse
 } else {
 }
 if (Test-Path -Path $env:TEMP/mass) {
-    Write-Host "Mass adında klasör buldum onu siliyorum."
+    Write-Host "Mass adinda klasör buldum onu siliyorum."
      Remove-Item $env:TEMP/mass -Recurse
  } else {
  }
@@ -12,11 +12,11 @@ Write-Host "MAS indiriyorum GitHub dan"
 Invoke-WebRequest https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip -OutFile $env:TEMP/mass.zip
 Write-Host "indirdim"
 
-Write-Host "Zipten çıkartıyorum"
+Write-Host "Zipten cikartiyorum"
 Expand-Archive $env:TEMP\mass.zip -DestinationPath $env:TEMP\mass
-Write-Host "Zipten çıkarttım"
+Write-Host "Zipten cikarttim"
 
-Write-Host "HWID çalıştırıyorum"
+Write-Host "HWID calistiriyorum"
 Set-Location $env:TEMP\mass\Microsoft-Activation-Scripts-master\MAS_1.4\Separate-Files-Version\Activators\HWID-KMS38_Activation
 Start-Process -FilePath HWID_Activation.cmd -Wait
 Pause
