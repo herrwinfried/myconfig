c18e404e0e4617654801cd938623c572db685d6c
winfried <ozgurarslln@icloud.com>
2021-12-18 16:55:12 +0300

    Test

--- mass.ps1
+++ mass.ps1
@@ -1,5 +1,5 @@
-$Folder = "$env:TEMP/mass.zip"
-if (Test-Path -Path $Folder) {
+$Folder = '$env:TEMP/mass.zip'
+if (Test-Path -Path $env:TEMP/mass.zip) {
    Write-Host "Mass adında zip buldum onu siliyorum."
     Remove-Item $env:TEMP/mass.zip -Recurse
 } else {
@@ -8,9 +8,9 @@ Write-Host "MAS indiriyorum GitHub dan"
 Invoke-WebRequest https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip -OutFile $env:TEMP/mass.zip
 Write-Host "indirdim"
 $Folder = "$env:TEMP/mass"
-if (Test-Path -Path $Folder) {
+if (Test-Path -Path $$env:TEMP/mass) {
    Write-Host "Mass adında klasör buldum onu siliyorum."
-    Remove-Item $env:TEMP/mass.zip -Recurse
+    Remove-Item $env:TEMP/mass -Recurse
 } else {
 }
 Write-Host "Zipten çıkartıyorum"
