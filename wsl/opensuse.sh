eb7f624483b9293fe926e9a4194abdf6c551ba4c
herrwinfried <52379312+herrwinfried@users.noreply.github.com>
2022-07-23 14:25:05 +0300

    Update opensuse.sh
    added font

--- wsl/opensuse.sh
+++ wsl/opensuse.sh
@@ -117,7 +117,11 @@ gsettings set org.gnome.desktop.interface icon-theme "Dracula"
 ############
 ############
 ############
-
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P /usr/share/fonts/truetype
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P /usr/share/fonts/truetype
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P /usr/share/fonts/truetype
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P /usr/share/fonts/truetype
+fc-cache
 
 }
 
