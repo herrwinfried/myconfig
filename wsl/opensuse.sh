1cf589fbf50001df9a03dbba656177a1ad09ba4a
Winfried <ozgurarslln@icloud.com>
2022-07-24 10:11:00 +0300

    Update OpenSUSE TW WSL
    Added mongoDB repository, updated Powershell method. Added some developer packs Added theme and font for GNOME

--- wsl/opensuse.sh
+++ wsl/opensuse.sh
@@ -46,12 +46,18 @@ sudo chown root:root /etc/zypp/repos.d/microsoft-prod.repo
 sudo rpm --import https://brave-browser-rpm-nightly.s3.brave.com/brave-core-nightly.asc
 sudo zypper addrepo https://brave-browser-rpm-nightly.s3.brave.com/x86_64/ brave-browser-nightly
 ##########################################
+sudo rpm --import https://www.mongodb.org/static/pgp/server-5.0.asc
+sudo zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/15/mongodb-org/5.0/x86_64/" mongodb
+##########################################
 sudo zypper --gpg-auto-import-keys refresh
 }
 function powershell {
 sudo zypper update
 sudo zypper in -y curl tar libicu60_2 libopenssl1_0_0
-curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.2.4/powershell-7.2.4-linux-x64.tar.gz -o /tmp/powershell.tar.gz
+sudo zypper in -y jq
+susepwshcore=$(curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest| jq -r ".assets[] | select(.name | test(\"linux-x64.tar.gz\")) | .browser_download_url")
+
+curl -L $susepwshcore -o /tmp/powershell.tar.gz
 sudo mkdir -p /opt/microsoft/powershell
 sudo tar -xzf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/
 sudo ln -s /opt/microsoft/powershell/pwsh /usr/bin/pwsh
@@ -72,7 +78,56 @@ sudo $tw_package install -y brave-browser-nightly
 
 function developerpackage {
     sudo $tw_package install -y nodejs-default npm-default python39 python39-pip dotnet-sdk-6.0 llvm-clang icu gcc gcc-c++ cmake rsync gdb ninja
-     sudo zypper install -y --type  pattern devel_basis
+    sudo zypper install -y --type  pattern devel_basis
+    ##Apache2, PHP8, MariaDB, MongoDB
+    sudo $tw_package install -y apache2 mariadb php8 php8-mysql apache2-mod_php8 mariadb mariadb-tools mongodb-org
+    ##flutter
+    sudo $tw_package install -y gtk3-devel java-18-openjdk
+    ##CONFIG
+    mkdir -p ~/data/db
+    a2enmod php8
+    sudo AddType application/x-httpd-php .php >> /etc/apache2/mod_mime-defaults.conf
+    ##CONFIG FINISH
+}
+function themesConfig {
+pwd5="$(pwd)"
+mkdir -p themeconfig
+cd themeconfig
+##GTK THEME
+wget https://github.com/dracula/gtk/archive/master.zip
+unzip master.zip
+mv gtk-master dracula
+mv dracula /usr/share/themes
+rm -rf *.zip
+##GTK ICON
+wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+unzip Dracula.zip
+mv Dracula /usr/share/icons/
+rm -rf *.zip
+##QT COLOR
+git clone https://github.com/dracula/qt5.git
+sudo cp qt5/Dracula.conf /usr/share/qt5ct/colors
+rm -rf qt5
+####################################################
+##Cursor
+sudo git clone https://github.com/dracula/gtk.git
+sudo cp gtk/kde/cursors/Dracula-cursors /usr/share/icons/
+sudo rm -rf gtk
+###########################################################
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+
+############
+############
+############
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P /usr/share/fonts/truetype
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P /usr/share/fonts/truetype
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P /usr/share/fonts/truetype
+sudo wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P /usr/share/fonts/truetype
+fc-cache
+############
+cd $pwd5
 }
 
 update
@@ -95,9 +150,10 @@ tw_gui_package="zypper --gpg-auto-import-keys"
 fi
 fi
 if [[ $tw_gui -eq 2 ]] || [[ $tw_gui -eq 1 ]]; then
-sudo $tw_gui_package install -y noto-sans-fonts gsettings-desktop-schemas xorg-x11-libs xorg-x11-server humanity-icon-theme patterns-fonts-fonts patterns-fonts-fonts_opt xorg-x11-fonts materia-gtk-theme gnome-tweaks
+sudo $tw_gui_package install -y noto-sans-fonts gsettings-desktop-schemas xorg-x11-libs xorg-x11-server humanity-icon-theme patterns-fonts-fonts patterns-fonts-fonts_opt xorg-x11-fonts materia-gtk-theme gnome-tweaks qt5ct
 sudo $tw_gui_package install -y Mesa-devel libOSMesa-devel libgthread-2_0-0 libts0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libevent-2_1-7 libminizip1 libpcre2-16-0
 sudo $tw_gui_package install -y Mesa-libva
+themesConfig
 fi
 
 }
