65e0577f9986d4455ce2582aae5d763bc1538979
Winfried <herrhofmannwinfried@gmail.com>
2022-11-20 09:13:18 +0300

    New Windows 11 Scripts

--- os/windows11/powershell/internallywsl.ps1
+++ os/windows11/powershell/internallywsl.ps1
@@ -1,15 +1,15 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-echo "I activate the Virtual Machine Platform."
+Write-Output "I activate the Virtual Machine Platform."
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-echo "I activate the Hyper-v."
+Write-Output "I activate the Hyper-v."
 DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /norestart
 bcdedit /set hypervisorlaunchtype auto
 
-echo "I activate the WSL."
+Write-Output "I activate the WSL."
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
-echo "Restart your computer."
+Write-Output "Restart your computer."
 
 pause
