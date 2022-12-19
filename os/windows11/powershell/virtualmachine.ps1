b5bd634f762df3017cb494a849d36a4f83817b7c
Winfried <herrhofmannwinfried@gmail.com>
2022-12-19 10:00:00 +0300

    It is divided into branches.

--- os/windows11/powershell/virtualmachine.ps1
+++
@@ -1,5 +0,0 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-
-Write-Output "I activate the Virtual Machine Platform."
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-pause
--- os/windows11/powershell/wslstore.ps1
+++
@@ -1,6 +0,0 @@
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-Write-Output "I activate the Virtual Machine Platform."
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-winget install --id 9P9TQF7MRM4R --accept-package-agreements --accept-source-agreements
-Write-Output "Restart your computer."
-pause
