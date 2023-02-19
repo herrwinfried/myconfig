b31c307f85a537b0fdfa6e7b1815929d61ad55bb
Winfried <herrhofmannwinfried@gmail.com>
2023-02-19 09:25:00 +0300

    Added code to auto-open some windows features, added Intel Unison added code for winbtrfs (may not work) Added OhMyPosh and added a few apps.

--- setup.ps1
+++ setup.ps1
@@ -6,77 +6,50 @@ $runnnx = "winget.exe install --id $Id --accept-package-agreements --accept-sour
         Invoke-Expression $runnnx
     }
 
-Start-Process PowerShell -verb runas "dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart; DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V-All /norestart; bcdedit /set hypervisorlaunchtype auto"
 $Host.UI.RawUI.WindowTitle = "HerrWinfried - MyConfig SETUP"
 
+Start-Process PowerShell -verb runas "dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V-All /all /norestart; dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart; dism.exe /online /enable-feature /featurename:HypervisorPlatform /all /norestart; bcdedit /set hypervisorlaunchtype auto; dism.exe /online /enable-feature /featurename:LegacyComponents /all /norestart; dism.exe /online /enable-feature /featurename:DirectPlay /all /norestart; dism.exe /online /enable-feature /featurename:Printing-XPSServices-Features /all /norestart; dism.exe /online /enable-feature /featurename:Printing-PrintToPDFServices-Features /all /norestart; "
+
 wingetx -Id Brave.Brave
-### ASUS && APPLE 
 #myasus
 wingetx -Id 9N7R5S6B0ZZH
-#Glidex
-wingetx -Id 9PLH2SV1DVK5
-#icloud
-wingetx -Id 9PKTQ5699M62
-#itunes
-wingetx -Id 9PB2MZ1ZMB1S
+#Intel Unison
 
-wingetx -Id Nvidia.GeForceExperience
-### WSL 
-#WSL
-wingetx -Id 9P9TQF7MRM4R
-#opensuse
-wingetx -Id 9MSSK2ZXXN11
+wingetx -Id 9PP9GZM2GN26
 
-### IDE & TEXT
-wingetx -Id Microsoft.VisualStudioCode
-wingetx -Id Microsoft.VisualStudio.2022.Community
-#Kate
-wingetx -Id 9NWMW7BB59HW
-#termius
-wingetx -Id 9NK1GDVPX09V
 
-wingetx -Id GitHub.GitHubDesktop
-wingetx -Id Git.Git
-wingetx -Id GitHub.GitLFS
+##########################################################WINBTRFS######################################################
+$BtrfsFolder = "$env:USERPROFILE\winbtrfs"
+# Folder Check
+if (Test-Path -Path $BtrfsFolder) {
+    Write-Host "I found folder named winbtrfs and deleted it." -ForegroundColor Red
+    Remove-Item "$env:USERPROFILE\winbtrfs" -Recurse
+}
 
-wingetx -Id MongoDB.Compass.Full
-wingetx -Id HeidiSQL.HeidiSQL
+New-Item "$env:USERPROFILE\winbtrfs" -ItemType Directory
 
-wingetx -Id OpenJS.NodeJS
-
-### Messenger & Printers
-wingetx -Id Discord.Discord 
-wingetx -Id Discord.Discord.PTB
-wingetx -Id Element.Element
-wingetx -Id Telegram.TelegramDesktop
-# Whatsapp beta
-wingetx -Id 9NBDXK71NK08
-#Kde connect
-wingetx -Id 9N93MRMSXBF0
-# HP Smart
-wingetx -Id 9WZDNCRFHWLH
-
-wingetx -Id Valve.Steam
-wingetx -Id EpicGames.EpicGamesLauncher
-
-#Bluetooth Audio Receiver
-wingetx -Id 9N9WCLWDQS5J
-
+Invoke-WebRequest https://github.com/maharmstone/btrfs/releases/download/v1.8.2/btrfs-1.8.2.zip -OutFile $env:USERPROFILE\winbtrfs\btrfs.zip
+Expand-Archive $env:USERPROFILE\winbtrfs\btrfs.zip -DestinationPath $env:USERPROFILE\winbtrfs\btrfs
+Set-Location $env:USERPROFILE\winbtrfs\btrfs\x64
+Get-ChildItem "$env:USERPROFILE\winbtrfs\btrfs\x64" -Recurse -Filter "*inf" | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install } 
+##########################################################WINBTRFS######################################################
 # Powershell
 wingetx -Id 9MZ1SNWT0N5D
 
 wingetx -Id JanDeDobbeleer.OhMyPosh
-if (!Test-Path -Path "$env:USERPROFILE\OhMyPosh") {
+
+
+if (-NOT (Test-Path -Path "$env:USERPROFILE\OhMyPosh")) {
 mkdir "$env:USERPROFILE\OhMyPosh"
 }
-if (Test-Path -Path "$env:USERPROFILE\OhMyPosh\powerlevel10k_rainbow.omp.json") {
+if (-NOT (Test-Path -Path "$env:USERPROFILE\OhMyPosh\powerlevel10k_rainbow.omp.json")) {
     Remove-Item "$env:USERPROFILE\OhMyPosh\powerlevel10k_rainbow.omp.json" -Recurse
     Invoke-WebRequest https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/powerlevel10k_rainbow.omp.json -OutFile $env:USERPROFILE\OhMyPosh\powerlevel10k_rainbow.omp.json
 } else {
     Invoke-WebRequest https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/powerlevel10k_rainbow.omp.json -OutFile $env:USERPROFILE\OhMyPosh\powerlevel10k_rainbow.omp.json
 }
 
-if (!Test-Path -Path "$env:USERPROFILE\Documents\PowerShell") {
+if (-NOT (Test-Path -Path "$env:USERPROFILE\Documents\PowerShell")) {
     mkdir "$env:USERPROFILE\Documents\PowerShell"
     }
 
@@ -94,12 +67,68 @@ if (!Test-Path -Path "$env:USERPROFILE\Documents\PowerShell") {
         Invoke-WebRequest https://raw.githubusercontent.com/herrwinfried/myconfig/windows/data/terminal.json -OutFile $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
     }
 
-wingetx -Id BlueStack.BlueStacks
+
+#Glidex
+wingetx -Id 9PLH2SV1DVK5
+#icloud
+wingetx -Id 9PKTQ5699M62
+#itunes
+wingetx -Id 9PB2MZ1ZMB1S
+
+wingetx -Id Nvidia.GeForceExperience
+### WSL 
+#WSL
+wingetx -Id 9P9TQF7MRM4R
+#opensuse
+wingetx -Id 9MSSK2ZXXN11
+
+### IDE & TEXT
+wingetx -Id Microsoft.VisualStudioCode
+wingetx -Id Microsoft.AzureDataStudio
+wingetx -Id Microsoft.VisualStudio.2022.Community
+#Kate
+wingetx -Id 9NWMW7BB59HW
+#termius
+wingetx -Id 9NK1GDVPX09V
+
+#wingetx -Id GitHub.GitHubDesktop
+wingetx -Id Git.Git
+wingetx -Id GitHub.GitLFS
+
+#wingetx -Id MongoDB.Compass.Full
+#wingetx -Id HeidiSQL.HeidiSQL
+
+wingetx -Id OpenJS.NodeJS
+
+
+wingetx -Id Discord.Discord 
+wingetx -Id Discord.Discord.PTB
+wingetx -Id Element.Element
+
+# Unigram / Telegram
+wingetx -Id 9N97ZCKPD60Q
+#wingetx -Id Telegram.TelegramDesktop
+
+# Whatsapp beta
+wingetx -Id 9NBDXK71NK08
+#Kde connect
+wingetx -Id KDE.KDEConnect
+# HP Smart
+wingetx -Id 9WZDNCRFHWLH
+
+wingetx -Id Valve.Steam
+wingetx -Id EpicGames.EpicGamesLauncher
+
+#Bluetooth Audio Receiver
+#wingetx -Id 9N9WCLWDQS5J
+
+# wingetx -Id BlueStack.BlueStacks
 wingetx -Id BinanceTech.Binance
+
 #auto dark mode
-wingetx -Id XP8JK4HZBVF435
+#wingetx -Id XP8JK4HZBVF435
 #WinDynamicDesktop
-wingetx -Id 9NM8N7DQ3Z5F
+#wingetx -Id 9NM8N7DQ3Z5F
 
 wingetx -Id OpenVPNTechnologies.OpenVPNConnect
 wingetx -Id Twilio.Authy
@@ -108,17 +137,21 @@ wingetx -Id 7zip.7zip
 #f.lux
 wingetx -Id 9N9KDPHV91JT
 
+wingetx -Id RevoUninstaller.RevoUninstaller
+
 wingetx -Id AnyDeskSoftwareGmbH.AnyDesk
 wingetx -Id TeamViewer.TeamViewer
 wingetx -Id GnuPG.Gpg4win
 wingetx -Id GnuPG.GnuPG
 wingetx -Id OBSProject.OBSStudio
 wingetx -Id Google.Drive
+wingetx -Id Mega.MEGASync
 wingetx -Id Microsoft.PowerToys
-wingetx -Id Microsoft.SQLServerManagementStudio
-wingetx -Id Microsoft.AzureDataStudio
+#wingetx -Id Microsoft.SQLServerManagementStudio
 wingetx -Id JetBrains.Toolbox
 
-
-
+#wingetx -Id RedHat.Podman 
+#wingetx -Id RedHat.Podman-Desktop
+wingetx -Id Docker.DockerDesktop
+wingetx -Id VMware.WorkstationPro
 
