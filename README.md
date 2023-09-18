8b3ae3006404aa7d317df57d6c9693e66cfca191
herrwinfried <ozgur.arslln@gmail.com>
2023-09-18 18:43:07 +0300

    Minor changes and adjustments

--- README.md
+++ README.md
@@ -7,18 +7,18 @@ Kendime göre yaptığım konfigürasyonlar var. Lütfen kendi bilincinize göre
 ## OpenSUSE Tumbleweed
 
 ```bash
-sudo sh -c 'zypper in -y wget curl git' && git clone https://github.com/herrwinfried/myconfig.git -b linux && mkdir -p myconfig/files && chmod +x myconfig/*.sh
+sudo sh -c 'zypper in -y wget curl git xdg-user-dirs' && git clone https://github.com/herrwinfried/myconfig.git -b linux && mkdir -p myconfig/files && xdg-user-dirs-update && chmod +x myconfig/scripts/*.sh
 ```
 ## fedora
 
 ```bash
-sudo sh -c 'dnf install -y wget curl git' && git clone https://github.com/herrwinfried/myconfig.git -b linux && mkdir -p myconfig/files && chmod +x myconfig/*.sh
+sudo sh -c 'dnf install -y wget curl git xdg-user-dirs' && git clone https://github.com/herrwinfried/myconfig.git -b linux && mkdir -p myconfig/files && xdg-user-dirs-update && chmod +x myconfig/scripts/*.sh
 ```
 
 ## debian
 
 ```bash
-sudo sh -c 'apt install -y wget curl git' && git clone https://github.com/herrwinfried/myconfig.git -b linux && mkdir -p myconfig/files && chmod +x myconfig/*.sh
+sudo sh -c 'apt install -y wget curl git xdg-user-dirs' && git clone https://github.com/herrwinfried/myconfig.git -b linux && mkdir -p myconfig/files && xdg-user-dirs-update && chmod +x myconfig/scripts/*.sh
 ```
 ### The file extension that the `files` folder supports *
 - .run
--- scripts/home/opensuse-tumbleweed/Package/base.sh
+++ scripts/home/opensuse-tumbleweed/Package/base.sh
@@ -4,7 +4,7 @@ Package_a="hwinfo screenfetch neofetch htop git git-lfs curl wget"
 Package_a+=" zsh bash-completion fish lsb-release opi e2fsprogs nano"
 Package_a+=" lzip unrar unzip java-20-openjdk"
 
-Package_a_Flatpak="flathub org.gtk.Gtk3theme.Adwaita-dark org.kde.KStyle.Adwaita//6.5 org.kde.PlatformTheme.QGnomePlatform//6.5"
+Package_a_Flatpak="flathub org.gtk.Gtk3theme.Breeze org.gtk.Gtk3theme.Adwaita-dark org.kde.KStyle.Adwaita//6.5 org.kde.PlatformTheme.QGnomePlatform//6.5"
 Package_a_Flatpak2="kdeapps org.kde.xwaylandvideobridge"
 
 Package_b="brave-browser microsoft-edge-stable fetchmsttfonts powerline-fonts"
@@ -13,13 +13,13 @@ Package_c_Flatpak="flathub org.onlyoffice.desktopeditors com.obsproject.Studio c
 Package_c_Flatpak+=" org.freedesktop.Platform.VulkanLayer.MangoHud//22.08 org.freedesktop.Platform.VulkanLayer.MangoHud//21.08"
 
 
-if ! checkwsl; then
-    Package_d_Flatpak="flathub com.valvesoftware.Steam net.lutris.Lutris com.usebottles.bottles com.heroicgameslauncher.hgl io.github.trigg.discover_overlay"
-    Package_d_Flatpak+=" com.valvesoftware.Steam.CompatibilityTool.Proton com.valvesoftware.Steam.CompatibilityTool.Proton-GE com.valvesoftware.Steam.Utility.MangoHud"
-    
+if ! checkwsl; then   
     Package_a_Flatpak+=" org.telegram.desktop io.github.mimbrero.WhatsAppDesktop"
     
     Package_b+=" discord flameshot AdobeICCProfiles anydesk teamviewer-suse noisetorch memtest86+ python311-pipx"
+
+    Package_d_Flatpak="flathub com.valvesoftware.Steam net.lutris.Lutris com.usebottles.bottles com.heroicgameslauncher.hgl io.github.trigg.discover_overlay net.davidotek.pupgui2"
+    Package_d_Flatpak+=" com.valvesoftware.Steam.CompatibilityTool.Proton com.valvesoftware.Steam.CompatibilityTool.Proton-GE com.valvesoftware.Steam.Utility.MangoHud"
 fi
 $SUDO $Package $PackageInstall $Package_a
 
