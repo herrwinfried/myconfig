87b2251501ee5c5033f0db4a553d719e05a42363
Winfried <ozgurarslln@icloud.com>
2024-03-28 14:18:58 +0300

    fix

--- scripts/home/opensuse-tumbleweed/Package/developer.sh
+++ scripts/home/opensuse-tumbleweed/Package/developer.sh
@@ -7,6 +7,7 @@ Package_a+=" qt6-base-devel qt6-declarative-devel"
 Package_a+=" dotnet-sdk-8.0 patterns-devel-mono-devel_mono"
 Package_a+=" patterns-devel-base-devel_rpm_build"
 Package_a+=" python312 python312-pip rsync build ninja"
+Package_a+=" nodejs npm-default"
 SUDO $Package $PackageInstall $Package_a
 
 dotnet new install Avalonia.Templates
